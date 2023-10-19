3938964f3bac63a05871c2a38f2aaebb3fdf7b93
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-19 11:41:49 +0530

    Update MyConnection.java

--- src/main/java/db/MyConnection.java
+++ src/main/java/db/MyConnection.java
@@ -1,2 +1,29 @@
-package db;public class MyConnection {
-}
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection=null;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/[Database Name]?useSSL=false", "root", "passwoed apna dalo");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Connection Successfully");
+        return connection;
+    }
+        public static void closeConnection() {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
