680bfdcfe1ff8f117d5f94ce0e65071ba5dd108a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-19 11:42:26 +0530

    Update User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,2 +1,27 @@
-package model;public class User {
+package model;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
