be41d418d645187096e18518b26f4a0b4dbdd702
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-19 11:44:46 +0530

    Update UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,2 +1,22 @@
-package service;public class UserService {
+package service;
+
+import dao.UserDAO;
+import model.User;
+
+import java.sql.SQLException;
+
+public class UserService {
+    public static Integer saveUser(User user){
+        try {
+            if(UserDAO.isExists(user.getEmail())) {
+                return 0;
+            } else {
+                return UserDAO.saveUser(user);
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
