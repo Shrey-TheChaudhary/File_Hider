291feace0c5cd10c6c2b7063fce2ffd6ce178a5a
Shrey Chaudhary <76205331+Shrey-TheChaudhary@users.noreply.github.com>
2023-10-19 11:45:12 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import views.Welcome;
+
+public class Main {
+    public static void main(String[] args) {
+        Welcome w =new Welcome();
+        do {
+            w.welcomeScreen();
+        } while (true);
+    }
 }
